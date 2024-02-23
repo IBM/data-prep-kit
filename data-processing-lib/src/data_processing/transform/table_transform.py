592b3fcf7d464b5a6c1b6bb2783fea01ea5bf3f4
David Wood <dawood@us.ibm.com>
2024-02-23 08:46:51 -0500

    Update table_transform.py

--- data-processing-lib/src/data_processing/transform/table_transform.py
+++ data-processing-lib/src/data_processing/transform/table_transform.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pyarrow as pa
 
 
@@ -7,9 +9,19 @@ class AbstractTableTransform:
     Sub-classes must provide the transform() method to provide the conversion of one table to 0 or more new tables.
     """
 
+    def __init__(self, config: dict[str, Any]):
+        """
+        By convention and in support of the ray runtime, we enable the transform to be initialized with a
+        transform-specific dictionary of configuration values.  The CLI arg definitions are provided
+        elsewhere to allow for the arguments to be parsed in a process separate from where this instance
+        is create.  This is because instance creation may include heavy lifting such as model loading.
+        """
+
+        pass
+
     def transform(self, table: pa.Table) -> list[pa.Table]:
         """
-        Converts input table into an output table
+        Converts input table into zero or more output tables.
         :param table: input table
         :return: a list of 0 or more converted tables.
         """
