d6d82d8e9a927c1b3037959df93333b70105389a
blublinsky <blublinsky@hotmail.com>
2024-03-01 18:25:21 +0000

    Add rebuild library command to makefile

--- data-processing-lib/src/data_processing/data_access/data_access.py
+++ data-processing-lib/src/data_processing/data_access/data_access.py
@@ -35,12 +35,14 @@ class DataAccess:
         """
         pass
 
-    def get_folder_files(self, path: str, extensions: list[str]) -> list[bytes]:
+    def get_folder_files(self, path: str, extensions: list[str] = None) -> dict[str, bytes]:
         """
-        Get a list of byte content of files
+        Get a list of byte content of files. The path here is an absolute path and can be anywhere.
+        The current limitation for S3 and Lakehouse is that it has to be in the same bucket
         :param path: file path
-        :param extensions: a list of file extensions to include
-        :return:
+        :param extensions: a list of file extensions to include. If None, then all files from this and
+                           child ones will be returned
+        :return: A dictionary of file names/binary content will be returned
         """
         pass
 
