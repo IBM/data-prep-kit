d9ed76693e513a9c8302c1cc6d7271b408a8c245
David Wood <dawood@us.ibm.com>
2024-03-11 11:37:36 -0400

    Add HF toekn config and use in noop_s3.
    Former-commit-id: 07e809f2dbedf7b9fbf29bb7e9d841de141638ca

--- transforms/universal/noop/src/noop_s3.py
+++ transforms/universal/noop/src/noop_s3.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from data_processing.ray import TransformLauncher
-from data_processing.utils import ParamsUtils
+from data_processing.utils import DPFConfig, ParamsUtils
 from noop_transform import NOOPTransformConfiguration
 
 
@@ -11,8 +11,8 @@ print(os.environ)
 launcher = TransformLauncher(transform_runtime_config=NOOPTransformConfiguration())
 # create parameters
 s3_cred = {
-    "access_key": os.environ.get("COS_ACCESS_KEY", "access"),
-    "secret_key": os.environ.get("COS_SECRET_KEY", "secret"),
+    "access_key": DPFConfig.S3_ACCESS_KEY,
+    "secret_key": DPFConfig.S3_SECRET_KEY,
     "cos_url": "https://s3.us-east.cloud-object-storage.appdomain.cloud",
 }
 s3_conf = {
