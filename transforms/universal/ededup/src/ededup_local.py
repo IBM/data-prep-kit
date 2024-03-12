35b73e3d9dc0de61d773ccf781c6847df6b1f526
David Wood <dawood@us.ibm.com>
2024-03-12 13:08:40 -0400

    Revert to using ../output  instead of ../test-data/output for *_local.py and add .gitignores for these dirs
    Former-commit-id: c614b54f97b46cb56ee8aea70c316e66b8c64c84

--- transforms/universal/ededup/src/ededup_local.py
+++ transforms/universal/ededup/src/ededup_local.py
@@ -11,7 +11,7 @@ from ededup_transform import EdedupTableTransformConfiguration
 launcher = TransformLauncher(transform_runtime_config=EdedupTableTransformConfiguration())
 # create parameters
 input_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "../test-data/input"))
-output_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "../test-data/output"))
+output_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "../output"))
 local_conf = {
     "input_folder": input_folder,
     "output_folder": output_folder,
