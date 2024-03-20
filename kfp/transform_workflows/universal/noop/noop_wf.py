80e4e640cb3256f41b09129fc8c286c4afb0ae44
mohammad.nassar@ibm.com <nassarmo@fundament01.sl.cloud9.ibm.com>
2024-03-20 07:47:35 -0500

    Fix conflicts.
    Signed-off-by: mohammad.nassar@ibm.com <nassarmo@fundament01.sl.cloud9.ibm.com>
    
    Former-commit-id: 5e1e594b7b78b6dac5fea74c8e4a7d9356d95871

--- kfp/transform_workflows/universal/noop/noop_wf.py
+++ kfp/transform_workflows/universal/noop/noop_wf.py
@@ -16,7 +16,7 @@ RUN_ID = uuid.uuid4().hex
 
 
 # components
-base_kfp_image = "us.icr.io/cil15-shared-registry/preprocessing-pipelines/kfp-data-processing:0.0.1"
+base_kfp_image = "us.icr.io/cil15-shared-registry/preprocessing-pipelines/kfp-data-processing:0.0.1-test3"
 # compute execution parameters. Here different tranforms might need different implementations. As
 # a result, insted of creating a component we are creating it in place here.
 compute_exec_params_op = comp.func_to_container_op(
@@ -38,10 +38,10 @@ TASK_NAME: str = "noop"
 )
 def noop(
     ray_name: str = "noop-kfp-ray",  # name of Ray cluster
-    ray_head_options: str = '{"cpu": 1, "memory": 4, "image": "us.icr.io/cil15-shared-registry/preprocessing-pipelines/noop-guf:0.0.1",\
+    ray_head_options: str = '{"cpu": 1, "memory": 2, "image": "us.icr.io/cil15-shared-registry/preprocessing-pipelines/noop:guftest",\
              "image_pull_secret": "prod-all-icr-io"}',
     ray_worker_options: str = '{"replicas": 2, "max_replicas": 2, "min_replicas": 2, "cpu": 2, "memory": 4, "image_pull_secret": "prod-all-icr-io",\
-            "image": "us.icr.io/cil15-shared-registry/preprocessing-pipelines/noop-guf:0.0.1"}',
+            "image": "us.icr.io/cil15-shared-registry/preprocessing-pipelines/noop:guftest"}',
     server_url: str = "http://kuberay-apiserver-service.kuberay.svc.cluster.local:8888",
     additional_params: str = '{"wait_interval": 2, "wait_cluster_ready_tmout": 400, "wait_cluster_up_tmout": 300, "wait_job_ready_tmout": 400, "wait_print_tmout": 30, "http_retries": 5}',
     noop_sleep_sec: int = 10,
