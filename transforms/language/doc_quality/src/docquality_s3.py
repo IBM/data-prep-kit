8bc9e50a9c6a660f2ff018d26447a74e4b165665
hajar.emami <hajar.emami@ibm.com>
2024-03-25 11:35:55 -0400

    run_s3
    Former-commit-id: 7f08e33f9d28d21e826c9ee223f87e83458c874c

--- transforms/language/doc_quality/src/docquality_s3.py
+++ transforms/language/doc_quality/src/docquality_s3.py
@@ -15,9 +15,14 @@ s3_cred = {
     "secret_key": DPFConfig.S3_SECRET_KEY,
     "url": "https://s3.us-east.cloud-object-storage.appdomain.cloud",
 }
+docq_params = {
+    "ft_lang": "en",
+    "bad_word_filepath": "../test-data/docq/ldnoobw/",
+    "MODEL_DIR": "../lm_sp/",
+}
 s3_conf = {
-    "input_folder": "cos-optimal-llm-pile/test/hajar/input/",
-    "output_folder": "cos-optimal-llm-pile/test/hajar/output/",
+    "input_folder": "cos-optimal-llm-pile/bluepile-processing/he/opensource/input/",
+    "output_folder": "cos-optimal-llm-pile/bluepile-processing/he/opensource/output/",
 }
 worker_options = {"num_cpus": 0.8}
 code_location = {"github": "github", "commit_hash": "12345", "path": "path"}
@@ -33,11 +38,8 @@ params = {
     "job_id": "job_id",
     "creation_delay": 0,
     "code_location": ParamsUtils.convert_to_ast(code_location),
-    "noop_sleep_sec": 5,
 }
-sys.argv = ParamsUtils.dict_to_req(d=params)
-# for arg in sys.argv:
-#     print(arg)
+sys.argv = ParamsUtils.dict_to_req(d=params | docq_params)
 
 # launch
 launcher.launch()
