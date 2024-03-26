dbe7c28093ace100d297a6bde0580a996d02d633
Xuan-Hong Dang <Xuan-Hong.Dang@ibm.com>
2024-03-25 22:44:52 -0400

    change default tokenizer to llama to avoid hf authentication
    Former-commit-id: b4af6e8ad52cdfee45a37fc8e27c5be687c28054

--- transforms/universal/tokenization/test/test_tokenization.py
+++ transforms/universal/tokenization/test/test_tokenization.py
@@ -8,18 +8,27 @@ from tokenization_transform import TokenizationTransform
 '''
 input table:
 '''
-table = pa.Table.from_pydict({"document_id": pa.array(["doc01", "doc02", "doc03"]),
-                                     "contents": pa.array(
-                                         ["This content is for doc01", "", "Another document content is for doc03"])})
+table = pa.Table.from_pydict({
+    "document_id": pa.array(["doc01", "doc02", "doc03"]),
+    "contents": pa.array(["This content is for doc01", "", "Another document content is for doc03"])
+    })
 
+
+# '''
+# expected output table as per HF's `bigcode/starcoder` tokenizer:
+# '''
+# tokens = pa.array([[2272,1795,438,436,3693,34,35],[20976,1825,1795,438,436,3693,34,37]])
+# document_id = pa.array(["doc01", "doc03"])
+# document_length = pa.array([25,37])
+# token_count = pa.array([7, 8])
 
 '''
 expected output table as per HF's `bigcode/starcoder` tokenizer:
 '''
-tokens = pa.array([[2272,1795,438,436,3693,34,35],[20976,1825,1795,438,436,3693,34,37]])
+tokens = pa.array([[1,910,2793,338,363,1574,29900,29896],[1,7280,1842,2793,338,363,1574,29900,29941]])
 document_id = pa.array(["doc01", "doc03"])
 document_length = pa.array([25,37])
-token_count = pa.array([7, 8])
+token_count = pa.array([8, 9])
 
 schema = pa.schema([("tokens", pa.list_(pa.int64())),
                     ("document_id", pa.string()),
@@ -34,7 +43,7 @@ expected_table = pa.Table.from_arrays([tokens,document_id,document_length,token_
 expected output metadata:
 '''
 expected_metadata_list = [
-    {'num_files': 1, 'num_rows': 3, 'num_tokenized_rows': 2, 'num_empty_rows': 1, 'num_tokens': 15, 'num_chars': 62},
+    {'num_files': 1, 'num_rows': 3, 'num_tokenized_rows': 2, 'num_empty_rows': 1, 'num_tokens': 17, 'num_chars': 62},
     {}]
 
 
@@ -42,7 +51,7 @@ expected_metadata_list = [
 Config (parameter settings) for the run:
 '''
 config = {
-            "tokenizer": "bigcode/starcoder",
+            "tokenizer": "hf-internal-testing/llama-tokenizer",
             "doc_id_column":"document_id",
             "doc_content_column":"contents",
             "text_lang": "en",
