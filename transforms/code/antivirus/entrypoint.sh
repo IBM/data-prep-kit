f43fe26a4bf71b458619b822b89c53c3cb9e9b4d
Takuya Goto <tkyg@jp.ibm.com>
2024-03-28 01:14:04 -0700

    wait for starting up clamd
    Former-commit-id: d7ef02c7adb4b72cda15f4c1b5a985d451b65e92

--- transforms/code/antivirus/entrypoint.sh
+++ transforms/code/antivirus/entrypoint.sh
@@ -4,6 +4,16 @@ set -euo pipefail
 
 # Start cland
 clamd
-
+# Wait for starting up clamd
+check="import clamd, time
+while True:
+  try:
+    clamd.ClamdUnixSocket().ping()
+    break
+  except:
+    print('waiting for starting up clamd')
+    time.sleep(5)
+"
+python -c "$check"
 # Run commands
 exec "$@"
