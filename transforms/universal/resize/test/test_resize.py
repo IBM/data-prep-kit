0940b95976864d8a7e00ce3dfbb55ce02c715ac0
David Wood <dawood@us.ibm.com>
2024-04-03 00:28:14 -0400

    Resize test improvements.
    Former-commit-id: 12445ca7dc183a8a861ae7e53254bd4591108177

--- transforms/universal/resize/test/test_resize.py
+++ transforms/universal/resize/test/test_resize.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 import pyarrow as pa
 from data_processing.data_access import DataAccess, DataAccessLocal
-from data_processing.test_support.abstract_test import get_tables_in_folder
+from data_processing.test_support import get_tables_in_folder
 from data_processing.test_support.transform.transform_test import AbstractTransformTest
 from resize_transform import ResizeTransform
 
--- transforms/universal/resize/test/test_resize_launch.py
+++ transforms/universal/resize/test/test_resize_launch.py
@@ -12,29 +12,30 @@ class TestRayResizeTransform(AbstractTransformLauncherTest):
 
     def get_test_transform_fixtures(self) -> list[tuple]:
         # The following based on 3 identical input files of about 39kbytes, and 200 rows
+        common_config = {"num_workers": 1}  # to make the output files repeatable.
         fixtures = []
         basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../test-data"))
 
         # Split into 4 or so files
-        config = {"resize_max_rows_per_table": 125}
+        config = {"resize_max_rows_per_table": 125} | common_config
         fixtures.append((ResizeTransformConfiguration(), config, basedir + "/input", basedir + "/expected-rows-125"))
 
         # # Merge into 2 or so files
-        config = {"resize_max_rows_per_table": 300}
+        config = {"resize_max_rows_per_table": 300} | common_config
         fixtures.append((ResizeTransformConfiguration(), config, basedir + "/input", basedir + "/expected-rows-300"))
 
         # # Merge all into a single table
-        config = {"resize_max_mbytes_per_table": 1}
+        config = {"resize_max_mbytes_per_table": 1} | common_config
         fixtures.append((ResizeTransformConfiguration(), config, basedir + "/input", basedir + "/expected-mbytes-1"))
 
         # # Merge the 1st 2 and some of the 2nd with the 3rd
-        config = {"resize_max_mbytes_per_table": 0.05}
+        config = {"resize_max_mbytes_per_table": 0.05} | common_config
         fixtures.append(
             (ResizeTransformConfiguration(), config, basedir + "/input", basedir + "/expected-mbytes-0.05")
         )
 
         # Split into 4 or so files
-        config = {"resize_max_mbytes_per_table": 0.02}
+        config = {"resize_max_mbytes_per_table": 0.02} | common_config
         fixtures.append(
             (ResizeTransformConfiguration(), config, basedir + "/input", basedir + "/expected-mbytes-0.02")
         )
