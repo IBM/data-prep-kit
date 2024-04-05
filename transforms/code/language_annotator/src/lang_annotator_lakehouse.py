7b4b150c0e7fa97f7bdb5c94191dae228144ca64
blublinsky <blublinsky@hotmail.com>
2024-04-05 12:45:32 +0100

    Completed transforms
    Former-commit-id: ee29afbb10d2922a33fac9d9bf6df0784d0f94fb

--- transforms/code/language_annotator/src/lang_annotator_lakehouse.py
+++ transforms/code/language_annotator/src/lang_annotator_lakehouse.py
@@ -1,9 +1,10 @@
 import os
 import sys
+from pathlib import Path
 
 from data_processing.ray import TransformLauncher
-from data_processing.utils import ParamsUtils
-from lang_filtering_transform import (
+from data_processing.utils import DPFConfig, ParamsUtils
+from lang_annotator_transform import (
     LangSelectorTransformConfiguration,
     lang_allowed_langs_file_key,
     lang_known_selector,
@@ -33,8 +34,26 @@ local_conf = {
     "input_folder": input_folder,
     "output_folder": output_folder,
 }
+
+s3_cred = {
+    "access_key": DPFConfig.S3_ACCESS_KEY,
+    "secret_key": DPFConfig.S3_SECRET_KEY,
+    "url": "https://s3.us-east.cloud-object-storage.appdomain.cloud",
+}
+# Configure lakehouse unit test tables
+lakehouse_config = {
+    "lh_environment": "STAGING",
+    "input_table": "code.ready_for_token",
+    "input_dataset": "",
+    "input_version": "main",
+    "output_table": "code.ready_for_token_langselect_test",
+    "output_path": "lh-test/tables/code/ready_for_token_langselect_test",
+    "token": DPFConfig.LAKEHOUSE_TOKEN,
+}
+
 worker_options = {"num_cpus": 0.8}
 code_location = {"github": "github", "commit_hash": "12345", "path": "path"}
+
 langselect_config = {
     lang_allowed_langs_file_key: selected_languages_file,
     lang_lang_column_key: language_column_name,
@@ -42,21 +61,27 @@ langselect_config = {
     lang_known_selector: True,
     "lang_select_local_config": ParamsUtils.convert_to_ast(local_conf),
 }
+
 params = {
+    # where to run
     "run_locally": True,
-    "max_files": -1,
-    "local_config": ParamsUtils.convert_to_ast(local_conf),
+    # Data access. Only required parameters are specified
+    "data_local_config": ParamsUtils.convert_to_ast(local_conf),
+    "data_s3_cred": ParamsUtils.convert_to_ast(s3_cred),
+    "data_lh_config": ParamsUtils.convert_to_ast(lakehouse_config),
+    # orchestrator
     "worker_options": ParamsUtils.convert_to_ast(worker_options),
-    "num_workers": 1,
-    "checkpointing": False,
+    "num_workers": 2,
     "pipeline_id": "pipeline_id",
     "job_id": "job_id",
     "creation_delay": 0,
     "code_location": ParamsUtils.convert_to_ast(code_location),
+    # language selection specific parameter
     **langselect_config,
 }
 
 if __name__ == "__main__":
+    Path(output_folder).mkdir(parents=True, exist_ok=True)
     sys.argv = ParamsUtils.dict_to_req(d=params)
     # launch
     launcher.launch()
