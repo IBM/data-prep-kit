66353b961edbcc5f3d43117724d396e5a82d5102
David Wood <dawood@us.ibm.com>
2024-04-05 15:47:17 -0400

    Add make test-locals for transforms and use in travis.
    Former-commit-id: a05ee9b332e9e9c5b733d33a7570e7ace494e009

--- transforms/universal/doc_id/src/doc_id_local.py
+++ transforms/universal/doc_id/src/doc_id_local.py
@@ -9,13 +9,6 @@ from doc_id_transform import DocIDTransform, DocIDTransformConfiguration
 
 # create parameters
 input_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "../test-data/input"))
-output_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "../output"))
-local_conf = {
-    "input_folder": input_folder,
-    "output_folder": output_folder,
-}
-# worker_options = {"num_cpus": 0.8}
-# code_location = {"github": "github", "commit_hash": "12345", "path": "path"}
 
 doc_id_params = {"doc_column": "contents", "hash_column": "doc_hash"}
 
@@ -24,7 +17,7 @@ if __name__ == "__main__":
     # Create and configure the transform.
     transform = DocIDTransform(doc_id_params)
     # Use the local data access to read a parquet table.
-    data_access = DataAccessLocal(local_conf)
+    data_access = DataAccessLocal()
     table = data_access.get_table(os.path.join(input_folder, "sample1.parquet"))
     print(f"input table: {table}")
     # Transform the table
@@ -34,5 +27,4 @@ if __name__ == "__main__":
     print(f"output metadata : {metadata}")
     column = table["doc_hash"]
     print(f"hashed column : {column}")
-    column = table["doc_index"]
     print(f"index column : {column}")
