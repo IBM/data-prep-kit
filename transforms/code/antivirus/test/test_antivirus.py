bbfccb44e70b2810fd4a03c365782c79d33249b6
TAKUYA GOTO <tkyg@jp.ibm.com>
2024-04-08 16:30:15 +0900

    update Makefile and test
    Former-commit-id: 0738b0ed474e5731bec72e125722e7a4f766245b

--- transforms/code/antivirus/test/test_antivirus.py
+++ transforms/code/antivirus/test/test_antivirus.py
@@ -9,23 +9,6 @@ from data_processing.utils import get_logger
 from antivirus_transform import AntivirusTransform
 
 
-INIT_TIMEOUT_SEC=60
-logger = get_logger(__name__)
-cd = clamd.ClamdUnixSocket()
-check_end = time.time() + INIT_TIMEOUT_SEC
-while True:
-    try:
-        cd.ping()
-        break
-    except Exception as err:
-        if time.time() < check_end:
-            logger.debug('waiting until clamd is ready...')
-            time.sleep(1)
-        else:
-            logger.error(f"clamd didn't become ready in {INIT_TIMEOUT_SEC} sec. Please check if clamav container is running.")
-            raise err
-del cd
-
 table = pa.Table.from_pydict({
             'document_id': ['ID_1', 'ID_2'],
             'contents': ['INNOCENT', 'X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'],
