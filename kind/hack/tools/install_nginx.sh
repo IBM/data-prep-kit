ba8c71c9be8b4a601d074bd11f84bb0e9d1598dc
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-05-02 15:52:51 -0300

    Refactor bash script for installing NGINX
    - Improved readability by consistent indentation and formatting.
    
    - Used more descriptive variable and function names.
    
    - Enhanced error handling with informative messages.
    
    - Added comments to clarify the purpose of different sections.
    
    - Quoted variables to prevent word splitting and pathname expansion.
    
    - Updated usage message for better clarity and consistency.
    
    Signed-off-by: Adriel007 <adrielsouzaandrade8@gmail.com>

--- kind/hack/tools/install_nginx.sh
+++ kind/hack/tools/install_nginx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-op=$1
+operation="$1"
 
 SLEEP_TIME="${SLEEP_TIME:-50}"
 MAX_RETRIES="${MAX_RETRIES:-10}"
@@ -8,46 +8,45 @@ EXIT_CODE=0
 
 source ../common.sh
 
-deploy() {
-	kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+deploy_nginx() {
+    kubectl apply -f "https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml"
 }
 
-wait(){
-	echo "Wait for nginx deployment."
-	wait_for_pods "ingress-nginx" "$MAX_RETRIES" "$SLEEP_TIME" || EXIT_CODE=$?
-
-	if [[ $EXIT_CODE -ne 0 ]]
-	then
-		echo "NGINX Deployment unsuccessful. Not all pods running"
-		exit $EXIT_CODE
-	fi
+wait_for_nginx_deployment() {
+    echo "Waiting for NGINX deployment..."
+    wait_for_pods "ingress-nginx" "$MAX_RETRIES" "$SLEEP_TIME" || EXIT_CODE=$?
+    
+    if [[ $EXIT_CODE -ne 0 ]]; then
+        echo "NGINX deployment unsuccessful. Not all pods are running."
+        exit "$EXIT_CODE"
+    fi
 }
 
-delete(){
-	kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+uninstall_nginx() {
+    kubectl delete -f "https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml"
 }
 
-usage(){
-        cat <<EOF
-"Usage: ./install_nginx.sh [cleanup|deploy-wait|deploy]"
+print_usage() {
+    cat <<EOF
+Usage: ./install_nginx.sh [cleanup|deploy-wait|deploy]
 EOF
 }
 
-case "$op" in
-	cleanup)
-		header_text "Uninstalling NGINX"
-		delete
-		;;
-	deploy-wait)
-		header_text "wait for NGINX deployment"
-		wait
-		;;
-	deploy)
-		header_text "Installing NGINX"
-		deploy
-		;;
-	*)
-		usage
-		;;
+case "$operation" in
+    cleanup)
+        header_text "Uninstalling NGINX"
+        uninstall_nginx
+        ;;
+    deploy-wait)
+        header_text "Deploying and waiting for NGINX"
+        deploy_nginx
+        wait_for_nginx_deployment
+        ;;
+    deploy)
+        header_text "Deploying NGINX"
+        deploy_nginx
+        ;;
+    *)
+        print_usage
+        ;;
 esac
-
