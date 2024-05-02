f445181225d941563d6ccdb6981c962ab5230391
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-05-02 15:40:55 -0300

    Refactor script for improved readability, portability, and robustness
    - Use descriptive variable names
    - Employ `case` statement for OS detection
    - Utilize `printf` over `echo` for consistent output
    - Check for `wget` dependency before usage
    - Ensure consistent variable quoting
    - Enhance error messages for better debugging
    
    Signed-off-by: Adriel007 <adrielsouzaandrade8@gmail.com>

--- kind/hack/common.sh
+++ kind/hack/common.sh
@@ -4,26 +4,34 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-os="unknown"
+current_os="unknown"
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	os="linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	os="darwin"
+case "$OSTYPE" in
+  linux-gnu)
+    current_os="linux"
+    ;;
+  darwin*)
+    current_os="darwin"
+    ;;
+esac
+
+if [[ "$current_os" == "unknown" ]]; then
+  printf "OS '%s' not supported. Aborting.\n" "$OSTYPE" >&2
+  exit 1
 fi
 
-if [[ "$os" == "unknown" ]]; then
-	echo "OS '$OSTYPE' not supported. Aborting." >&2
-	exit 1
+if [ ! -f "${ROOT_DIR}/hack/.helper-functions.sh" ]; then
+  printf "Download helper-functions.sh\n"
+  if command -v wget &>/dev/null; then
+    TEKTON_KFP_SERVER_VERSION=1.8.1
+    wget "https://raw.githubusercontent.com/kubeflow/kfp-tekton/v${TEKTON_KFP_SERVER_VERSION}/scripts/deploy/iks/helper-functions.sh" -O "${ROOT_DIR}/hack/.helper-functions.sh"
+  else
+    printf "Error: wget not found. Aborting.\n" >&2
+    exit 1
+  fi
 fi
 
-if [ ! -f ${ROOT_DIR}/hack/.helper-functions.sh ]; then
-	echo "Download helper-functions.sh"
-	TEKTON_KFP_SERVER_VERSION=1.8.1
-	wget https://raw.githubusercontent.com/kubeflow/kfp-tekton/v${TEKTON_KFP_SERVER_VERSION}/scripts/deploy/iks/helper-functions.sh -O ${ROOT_DIR}/hack/.helper-functions.sh
-fi
-source ${ROOT_DIR}/hack/.helper-functions.sh
-
+source "${ROOT_DIR}/hack/.helper-functions.sh"
 
 # Turn colors in this script off by setting the NO_COLOR variable in your
 # environment to any value:
@@ -31,13 +39,13 @@ source ${ROOT_DIR}/hack/.helper-functions.sh
 # $ NO_COLOR=1 test.sh
 NO_COLOR=${NO_COLOR:-""}
 if [ -z "$NO_COLOR" ]; then
-	header=$'\e[1;33m'
-	reset=$'\e[0m'
+  header=$'\e[1;33m'
+  reset=$'\e[0m'
 else
-	header=''
-	reset=''
+  header=''
+  reset=''
 fi
 
 function header_text {
-	echo "$header$*$reset"
+  printf "%s%s%s\n" "$header" "$*" "$reset"
 }
