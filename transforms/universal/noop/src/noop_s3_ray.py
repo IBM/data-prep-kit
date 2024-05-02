54246fac78ee8e449f104e23e433eaea45f2ecfd
blublinsky <blublinsky@hotmail.com>
2024-05-02 15:09:55 +0100

    pure-python implementation

--- transforms/universal/noop/src/noop_s3_ray.py
+++ transforms/universal/noop/src/noop_s3_ray.py
@@ -15,12 +15,12 @@ import sys
 
 from data_processing.ray import TransformLauncherRay
 from data_processing.utils import ParamsUtils
-from noop_transform import NOOPTransformConfiguration
+from noop_transform import NOOPTransformConfigurationRay
 
 
 print(os.environ)
 # create launcher
-launcher = TransformLauncherRay(transform_runtime_config=NOOPTransformConfiguration())
+launcher = TransformLauncherRay(transform_runtime_config=NOOPTransformConfigurationRay())
 # create parameters
 s3_cred = {
     "access_key": "localminioaccesskey",
