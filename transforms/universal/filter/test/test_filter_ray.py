85e1bc3713f69bd3203801684611182b8dd61dd1
blublinsky <blublinsky@hotmail.com>
2024-05-03 16:25:35 +0100

    transforms fixes

--- transforms/universal/filter/test/test_filter_ray.py
+++ transforms/universal/filter/test/test_filter_ray.py
@@ -56,7 +56,7 @@ class TestRayFilterTransform(AbstractTransformLauncherTest):
 
         fixtures.append(
             (
-                FilterTransformConfiguration(),
+                FilterTransformConfigurationRay(),
                 {
                     filter_criteria_cli_param: [
                         "docq_total_words > 100 AND docq_total_words < 200",
@@ -72,7 +72,7 @@ class TestRayFilterTransform(AbstractTransformLauncherTest):
 
         fixtures.append(
             (
-                FilterTransformConfiguration(),
+                FilterTransformConfigurationRay(),
                 {
                     filter_criteria_cli_param: [],
                     filter_logical_operator_cli_param: filter_logical_operator_default,
@@ -85,7 +85,7 @@ class TestRayFilterTransform(AbstractTransformLauncherTest):
 
         fixtures.append(
             (
-                FilterTransformConfiguration(),
+                FilterTransformConfigurationRay(),
                 {
                     filter_criteria_cli_param: [
                         "date_acquired BETWEEN '2023-07-04' AND '2023-07-08'",
@@ -101,7 +101,7 @@ class TestRayFilterTransform(AbstractTransformLauncherTest):
 
         fixtures.append(
             (
-                FilterTransformConfiguration(),
+                FilterTransformConfigurationRay(),
                 {
                     filter_criteria_cli_param: [
                         "document IN ('CC-MAIN-20190221132217-20190221154217-00305.warc.gz', 'CC-MAIN-20200528232803-20200529022803-00154.warc.gz', 'CC-MAIN-20190617103006-20190617125006-00025.warc.gz')",
