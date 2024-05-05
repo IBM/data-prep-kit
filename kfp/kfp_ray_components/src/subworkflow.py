666e118354b0bb4aeaaa6fbf4fb788cd84822423
Revital Sur <eres@il.ibm.com>
2024-05-05 12:05:31 +0300

    Minor fix.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- kfp/kfp_ray_components/src/subworkflow.py
+++ kfp/kfp_ray_components/src/subworkflow.py
@@ -94,7 +94,7 @@ def invoke_sub_workflow(
     def _skip_task(prms) -> bool:
         # if there is a skip parameter and it is True then skip the task
         if "skip" in prms:
-            if prms.get("skip", "False").lower() == "true":
+            if prms.get("skip", "False").lower() in ('true', 'yes'):
                 return True
         return False
 
