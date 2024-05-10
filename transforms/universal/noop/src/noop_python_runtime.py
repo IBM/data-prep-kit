d499758f4811392e82433763c4df8cc5d71c074e
David Wood <dawood@us.ibm.com>
2024-05-10 18:24:32 -0400

    fix noop_python_runtime.py to not use RayTransformConfiguration
    Signed-off-by: David Wood <dawood@us.ibm.com>

--- transforms/universal/noop/src/noop_python_runtime.py
+++ transforms/universal/noop/src/noop_python_runtime.py
@@ -1,9 +1,9 @@
 from data_processing.runtime.pure_python import PythonTransformLauncher
-from noop_transform import NOOPRayTransformConfiguration, logger
+from noop_transform import NOOPTransformConfiguration, logger
 
 
 if __name__ == "__main__":
     # launcher = NOOPRayLauncher()
-    launcher = PythonTransformLauncher(NOOPRayTransformConfiguration())
+    launcher = PythonTransformLauncher(NOOPTransformConfiguration())
     logger.info("Launching noop transform")
     launcher.launch()
