96bb523c4c3c2b2121415e881c9a8eaf6079c4e9
blublinsky <blublinsky@hotmail.com>
2024-05-19 10:59:06 +0100

    removed circular dependency

--- data-processing-lib/ray/src/data_processing/utils/__init__.py
+++ data-processing-lib/ray/src/data_processing/utils/__init__.py
@@ -1,5 +1,5 @@
-from data_processing.utils.cli_utils import GB, KB, MB, CLIArgumentProvider, str2bool
-from data_processing.utils.params_utils import ParamsUtils
 from data_processing.utils.config import DPKConfig, add_if_missing
+from data_processing.utils.cli_utils import GB, KB, MB, CLIArgumentProvider, str2bool
 from data_processing.utils.log import get_logger
+from data_processing.utils.params_utils import ParamsUtils
 from data_processing.utils.transform_utils import TransformUtils, RANDOM_SEED, LOCAL_TO_DISK
