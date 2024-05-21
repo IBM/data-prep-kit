a95182b49c230da7504028680d23c0d9e66cd15d
Revital Sur <eres@il.ibm.com>
2024-05-21 13:02:34 +0300

    Enable kfp in GH action for testing workflows.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- kind/hack/populate_minio.sh
+++ kind/hack/populate_minio.sh
@@ -11,15 +11,15 @@ echo "creating test bucket"
 mc mb kfp/test
 echo "copying data"
 # code modules
-mc cp --recursive ${ROOT_DIR}/../transforms/code/code_quality/test-data/input/ kfp/test/code_quality/input
-mc cp --recursive ${ROOT_DIR}/../transforms/code/proglang_select/test-data/input/ kfp/test/proglang_select/input
-mc cp --recursive ${ROOT_DIR}/../transforms/code/proglang_select/test-data/languages/ kfp/test/proglang_select/languages
-mc cp --recursive ${ROOT_DIR}/../transforms/code/malware/test-data/input/ kfp/test/malware/input
+mc cp --recursive ${ROOT_DIR}/../transforms/code/code_quality/ray/test-data/input/ kfp/test/code_quality/input
+mc cp --recursive ${ROOT_DIR}/../transforms/code/proglang_select/ray/test-data/input/ kfp/test/proglang_select/input
+mc cp --recursive ${ROOT_DIR}/../transforms/code/proglang_select/ray/test-data/languages/ kfp/test/proglang_select/languages
+mc cp --recursive ${ROOT_DIR}/../transforms/code/malware/ray/test-data/input/ kfp/test/malware/input
 # universal
-mc cp --recursive ${ROOT_DIR}/../transforms/universal/doc_id/test-data/input/ kfp/test/doc_id/input
-mc cp --recursive ${ROOT_DIR}/../transforms/universal/ededup/test-data/input/ kfp/test/ededup/input
-mc cp --recursive ${ROOT_DIR}/../transforms/universal/fdedup/test-data/input/ kfp/test/fdedup/input
-mc cp --recursive ${ROOT_DIR}/../transforms/universal/filter/test-data/input/ kfp/test/filter/input
-mc cp --recursive ${ROOT_DIR}/../transforms/universal/noop/test-data/input/ kfp/test/noop/input
-mc cp --recursive ${ROOT_DIR}/../transforms/universal/tokenization/test-data/ds01/input/ kfp/test/tokenization/ds01/input
+mc cp --recursive ${ROOT_DIR}/../transforms/universal/doc_id/ray/test-data/input/ kfp/test/doc_id/input
+mc cp --recursive ${ROOT_DIR}/../transforms/universal/ededup/ray/test-data/input/ kfp/test/ededup/input
+mc cp --recursive ${ROOT_DIR}/../transforms/universal/fdedup/ray/test-data/input/ kfp/test/fdedup/input
+mc cp --recursive ${ROOT_DIR}/../transforms/universal/filter/ray/test-data/input/ kfp/test/filter/input
+mc cp --recursive ${ROOT_DIR}/../transforms/universal/noop/ray/test-data/input/ kfp/test/noop/input
+mc cp --recursive ${ROOT_DIR}/../transforms/universal/tokenization/ray/test-data/ds01/input/ kfp/test/tokenization/ds01/input
 
