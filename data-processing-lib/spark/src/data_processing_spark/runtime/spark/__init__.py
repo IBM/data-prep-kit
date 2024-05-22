f1bbc89c30b3b967daf09a37661c91ecc9c63a9c
David Wood <dawood@us.ibm.com>
2024-05-22 15:03:33 -0400

    fix spark __init__.py _key to _cli
    Signed-off-by: David Wood <dawood@us.ibm.com>

--- data-processing-lib/spark/src/data_processing_spark/runtime/spark/__init__.py
+++ data-processing-lib/spark/src/data_processing_spark/runtime/spark/__init__.py
@@ -1,8 +1,8 @@
 from data_processing_spark.runtime.spark.runtime_config import SparkTransformRuntimeConfiguration
 from data_processing_spark.runtime.spark.spark_execution_config import (
     SparkExecutionConfiguration,
-    kube_config_path_key,
-    local_config_path_key,
+    kube_config_path_cli,
+    local_config_path_cli,
 )
 from data_processing_spark.runtime.spark.spark_launcher import SparkTransformLauncher
 from data_processing_spark.runtime.spark.spark_transform_config import SparkTransformConfiguration
