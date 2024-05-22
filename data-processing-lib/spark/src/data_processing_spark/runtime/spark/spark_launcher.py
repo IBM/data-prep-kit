c47dca955181a522b695f69777bfb5b287762bb4
Constantin M Adam <cmadam@us.ibm.com>
2024-05-21 23:14:20 -0400

    First running version of noop local in Spark

--- data-processing-lib/spark/src/data_processing_spark/runtime/spark/spark_launcher.py
+++ data-processing-lib/spark/src/data_processing_spark/runtime/spark/spark_launcher.py
@@ -1,6 +1,10 @@
 import argparse
+import os
+import socket
 from argparse import ArgumentParser
+from typing import Union
 
+import yaml
 from data_processing.data_access import (
     DataAccess,
     DataAccessFactory,
@@ -15,6 +19,7 @@ from data_processing_spark.runtime.spark.runtime_config import (
     SparkTransformRuntimeConfiguration,
 )
 from data_processing_spark.runtime.spark.spark_transform import AbstractSparkTransform
+from pyspark.sql import DataFrame, SparkSession
 
 
 logger = get_logger(__name__)
@@ -52,13 +57,129 @@ class SparkTransformLauncher(AbstractTransformLauncher):
         self._stop_spark()
 
     def _start_spark(self):
-        pass
+        server_port_https = int(os.getenv("KUBERNETES_SERVICE_PORT_HTTPS", "-1"))
+        if server_port_https == -1:
+            # we are running locally, use the spark_profile_local.yaml file for configuration
+            config_filepath = os.path.abspath(
+                os.path.join(
+                    os.path.dirname(__file__), "../../../../", "test-data", "config", "spark_profile_local.yaml"
+                )
+            )
+            with open(config_filepath, "r") as config_fp:
+                spark_config = yaml.safe_load(os.path.expandvars(config_fp.read()))
+            app_name = spark_config.get("spark.app.name", "my-spark-app")
+            self.spark = SparkSession.builder.appName(app_name).config(map=spark_config).getOrCreate()
+        else:
+            # we are running in Kubernetes, use spark_profile.yaml and
+            # environment variables for configuration
+
+            server_port = os.environ["KUBERNETES_SERVICE_PORT"]
+            master_url = f"k8s://https://kubernetes.default:{server_port}"
+
+            # Read Spark configuration profile
+            config_filepath = os.path.abspath(
+                os.path.join(os.path.dirname(__file__), "..", "config", "spark_profile.yaml")
+            )
+            with open(config_filepath, "r") as config_fp:
+                spark_config = yaml.safe_load(os.path.expandvars(config_fp.read()))
+            spark_config["spark.submit.deployMode"] = "client"
+
+            # configure the executor pods from template
+            executor_pod_template_file = os.path.join(
+                os.path.dirname(__file__),
+                "templates",
+                "spark-executor-pod-template.yaml",
+            )
+            spark_config["spark.kubernetes.executor.podTemplateFile"] = executor_pod_template_file
+            spark_config["spark.kubernetes.container.image.pullPolicy"] = "Always"
+
+            # Pass the driver IP address to the workers for callback
+            myservice_url = socket.gethostbyname(socket.gethostname())
+            spark_config["spark.driver.host"] = myservice_url
+            spark_config["spark.driver.bindAddress"] = "0.0.0.0"
+
+            spark_config["spark.decommission.enabled"] = True
+
+            logger.info(f"Launching Spark Session with configuration\n" f"{yaml.dump(spark_config, indent=2)}")
+            app_name = spark_config.get("spark.app.name", "my-spark-app")
+            self.spark = (
+                SparkSession.builder.master(master_url).appName(app_name).config(map=spark_config).getOrCreate()
+            )
+
+        # configure S3 for Spark Session
+        hconf = self.spark.sparkContext._jsc.hadoopConfiguration()
+        hconf.set("com.amazonaws.services.s3.enableV4", "true")
+        hconf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        # hconf.set(
+        #     "fs.s3a.aws.credentials.provider",
+        #     "com.amazonaws.auth.InstanceProfileCredentialsProvider,com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
+        # )
+        hconf.set("fs.AbstractFileSystem.s3a.impl", "org.apache.hadoop.fs.s3a.S3A")
 
     def _stop_spark(self):
-        pass
+        self.spark.stop()
+
+    def _read_data(self, input_data_url: Union[list[str], str], data_type: str) -> DataFrame:
+
+        if isinstance(input_data_url, str) and input_data_url.startswith("s3://"):
+            hconf = self.spark.sparkContext._jsc.hadoopConfiguration()
+            access_key = os.getenv("AWS_ACCESS_KEY_ID_IN")
+            secret_key = os.getenv("AWS_SECRET_ACCESS_KEY_IN")
+            endpoint = os.getenv("AWS_ENDPOINT_URL_IN")
+            hconf.set("fs.s3a.access.key", access_key)
+            hconf.set("fs.s3a.secret.key", secret_key)
+            hconf.set("fs.s3a.endpoint", endpoint)
+            logger.info("Applied user-provided credential to S3")
+            # for some reason, Hadoop can only process S3 urls if they start with s3a://, not s3://
+            input_data_url = input_data_url.replace("s3://", "s3a://")
+
+        read_cmd = self.spark.read
+        if data_type == "parquet":
+            read_cmd = read_cmd.parquet
+        elif data_type == "csv":
+            read_cmd = read_cmd.csv
+        elif data_type == "json":
+            read_cmd = read_cmd.json
+        elif data_type == "orc":
+            read_cmd = read_cmd.orc
+        else:
+            read_cmd = read_cmd.text
+
+        spark_df = read_cmd(",".join(input_data_url) if isinstance(input_data_url, list) else input_data_url)
+        return spark_df
+
+    def _write_data(self, spark_df: DataFrame, output_data_url: str, data_type: str):
+        if isinstance(output_data_url, str) and output_data_url.startswith("s3://"):
+            hconf = self.spark.sparkContext._jsc.hadoopConfiguration()
+            access_key = os.getenv("AWS_ACCESS_KEY_ID_OUT")
+            secret_key = os.getenv("AWS_SECRET_ACCESS_KEY_OUT")
+            endpoint = os.getenv("AWS_ENDPOINT_URL_OUT")
+            hconf.set("fs.s3a.access.key", access_key)
+            hconf.set("fs.s3a.secret.key", secret_key)
+            hconf.set("fs.s3a.endpoint", endpoint)
+            logger.info("Applied user-provided credential to S3")
+            # for some reason, Hadoop can only process S3 urls if they start with s3a://, not s3://
+            output_data_url = output_data_url.replace("s3://", "s3a://")
+
+        write_cmd = spark_df.write.mode("overwrite")
+        if data_type == "parquet":
+            write_cmd = write_cmd.parquet
+        elif data_type == "csv":
+            write_cmd = write_cmd.csv
+        elif data_type == "json":
+            write_cmd = write_cmd.json
+        elif data_type == "orc":
+            write_cmd = write_cmd.orc
+        else:
+            write_cmd = write_cmd.text
+        write_cmd(output_data_url)
 
     def _run_transform(self, data_access: DataAccess, transform: AbstractSparkTransform):
-        pass
+        files, _ = data_access.get_files_to_process()
+        spark_df = self._read_data(files, data_type="parquet")
+        res_spark_df, metadata = transform.transform(spark_df)
+        data_access.save_job_metadata(metadata)
+        self._write_data(res_spark_df[0], data_access.get_output_folder(), data_type="parquet")
 
     def _get_args(self) -> bool:
         parser = argparse.ArgumentParser(
