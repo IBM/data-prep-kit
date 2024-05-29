7ee911a564d44833172a3027f0f37798e914c303
Mohammad Nassar <mohammad.nassar@ibm.com>
2024-05-29 05:41:25 -0500

    Update run.sh script.
    Signed-off-by: Mohammad Nassar <mohammad.nassar@ibm.com>

--- kfp/pipeline_generator/run.sh
+++ kfp/pipeline_generator/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-DIST_DIR=$1
-ROOT_DIR=${PWD}/..
+DEF_FILE=$1
+DIST_DIR=$2
+ROOT_DIR=${PWD}
 VENV_ACTIVATE=${ROOT_DIR}/../kfp_support_lib/venv/bin/activate
 
 source $VENV_ACTIVATE
 mkdir -p ${ROOT_DIR}/${DIST_DIR}/
-python3 pipeline_generator.py -c example/pipeline_definitions.yaml -od ${ROOT_DIR}/${DIST_DIR}/
+python3 pipeline_generator.py -c ${DEF_FILE} -od ${ROOT_DIR}/${DIST_DIR}/
