c1e39d7f74a9db2b95396f3ed97e0e9e54203a6f
Mohammad Nassar <mohammad.nassar@ibm.com>
2024-05-29 06:30:42 -0500

    Small fix.
    Signed-off-by: Mohammad Nassar <mohammad.nassar@ibm.com>

--- kfp/pipeline_generator/run.sh
+++ kfp/pipeline_generator/run.sh
@@ -3,9 +3,7 @@
 DEF_FILE=$1
 DIST_DIR=$2
 ROOT_DIR=${PWD}
-VENV_ACTIVATE=${ROOT_DIR}/../kfp_support_lib/venv/bin/activate
 
-source $VENV_ACTIVATE
 mkdir -p ${ROOT_DIR}/${DIST_DIR}/
 python3 -m venv venv
 source venv/bin/activate
