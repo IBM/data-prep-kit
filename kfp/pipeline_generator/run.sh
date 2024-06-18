d02693ec309a8f430ce6802d6794ec496d339a7e
Sai <sai@Sais-MacBook-Pro.local>
2024-06-18 20:30:12 +0530

    updated pipeline_generator.
    Signed-off-by: Sai <sai@Sais-MacBook-Pro.local>

--- kfp/pipeline_generator/run.sh
+++ kfp/pipeline_generator/run.sh
@@ -8,4 +8,6 @@ mkdir -p ${ROOT_DIR}/${DIST_DIR}/
 python3 -m venv venv
 source venv/bin/activate
 pip install pre-commit
+pip install jinja2
+# python3 pipeline_generator.py -c ${DEF_FILE} -od ${ROOT_DIR}/${DIST_DIR}/
 python3 pipeline_generator.py -c ${DEF_FILE} -od ${ROOT_DIR}/${DIST_DIR}/
