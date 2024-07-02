af5a29547aec7facbd132903917742b3ee9c9898
Revital Sur <eres@il.ibm.com>
2024-07-02 12:41:11 +0300

    Handle latest tag.
    Signed-off-by: Revital Sur <eres@il.ibm.com>
    Signed-off-by: SowmyaLR <lrsowmya@lrsowmyas-MacBook-Pro.local>

--- scripts/transforms/update_workflow_tags.sh
+++ scripts/transforms/update_workflow_tags.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ $# != 2 ]]; then
+set -x
+
+if [[ $# != 3 ]]; then
   cat << EOF
-	"Incorrect number of parameters provided. The required parameters are versions_file and pipeline_path. 
+	$1 $2 
+	"Incorrect number of parameters provided. The required parameters are versions_file, pipeline_path and docker_image_version. 
 EOF
   exit 1
 fi
 
 versions_file=$1
 pipeline_path=$2
+docker_image_version=$3
 
 # Modify the tasks tags as defined in the versions file
 while IFS= read -r line; do 
@@ -19,6 +23,5 @@ while IFS= read -r line; do
 	VERSION_NAME=$(echo $line |cut -d "=" -f 1)
 	DOCKER_IMAGE_NAME=$(echo $line |cut -d "=" -f 1 |sed "s/_VERSION//" |tr '[:upper:]' '[:lower:]')
 	DOCKER_IMAGE_NAME=$(echo $DOCKER_IMAGE_NAME |sed "s/_ray$/\-ray/" | sed "s/_spark$/\-spark/" | sed "s/_parquet$/\-parquet/")
-	DOCKER_IMAGE_VERSION=$(eval echo ${!VERSION_NAME})
-	sed -i.back "s/data-prep-kit\/$DOCKER_IMAGE_NAME:.*/data-prep-kit\/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION\"/" $pipeline_path
+	sed -i.back "s/data-prep-kit\/$DOCKER_IMAGE_NAME:.*/data-prep-kit\/$DOCKER_IMAGE_NAME:$docker_image_version\"/" $pipeline_path
 done < $versions_file
