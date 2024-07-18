695b84214f2ca108327353c7ca5a3c020678297a
Constantin M Adam <cmadam@us.ibm.com>
2024-07-18 00:55:24 -0400

    Scalable filtering; more logging
    Signed-off-by: Constantin M Adam <cmadam@us.ibm.com>

--- transforms/universal/fdedup/spark/src/fd_clean_data.py
+++ transforms/universal/fdedup/spark/src/fd_clean_data.py
@@ -62,9 +62,16 @@ class FDCleanData(SparkTransformerRuntime):
 
             spark_df_explode = spark_df.withColumn("rm_doc_id", explode(col("doc_ids")))
             docs_to_remove_list.extend([int(row.rm_doc_id) for row in spark_df_explode.select("rm_doc_id").collect()])
-        return docs_to_remove_list
+        return list(set(docs_to_remove_list))
 
     def clean_data(self, docs2remove_list: list):
+        logging.info(f"Cleaning data with {len(docs2remove_list)} documents to remove")
+        remove_ids_df = self.spark.createDataFrame(
+            [(doc_id,) for doc_id in docs2remove_list],
+            [self.document_id_column],
+        )
+        logging.info(f"Created remove_ids_df dataframe with {remove_ids_df.count()} rows")
+        # read the original data
         self.input_files, self.file_stats = self.list_files(self.input_path, self.file_ext)
         file_batcher = SparkFileBatcher(
             self.input_files,
@@ -73,24 +80,29 @@ class FDCleanData(SparkTransformerRuntime):
             os.path.join(self.output_path, "checkpoint.txt"),
         )
         logging.info("Starting fuzzy dedupe data clean")
-
+        total_num_documents = 0
+        num_documents_left_after_removal = 0
         # Loop through batches until all files processed
         while True:
             # Get next batch
-            input_batch, batch_size = file_batcher.next_batch()
+            input_batch, _ = file_batcher.next_batch()
             if not input_batch:
                 break
-
             logging.info(f"Processing batch {file_batcher.current_batch_index} out of {file_batcher.total_batches}")
-
             # read raw data
-            spark_df = self.read_data(input_batch, self.file_ext)
-
+            input_df = self.read_data(input_batch, self.file_ext)
+            batch_total_docs = input_df.count()
+            logging.info(f"{batch_total_docs} documents before filtering")
+            total_num_documents += batch_total_docs
             # Filter out rows where the 'id' column is in the given list
-            final_sdf = spark_df.filter(~spark_df[self.document_id_column].isin(docs2remove_list))
-
+            filtered_df = input_df.join(remove_ids_df, self.document_id_column, "left_anti")
+            batch_documents_left = filtered_df.count()
+            logging.info(f"{batch_documents_left} documents left after filtering")
+            num_documents_left_after_removal += batch_documents_left
             # write cleaned data
-            self.write_data(final_sdf, self.cleaned_output_path, self.file_ext)
+            self.write_data(filtered_df, self.cleaned_output_path, self.file_ext)
+            logging.info("Wrote filtered data")
+        logging.info(f"Fuzzy dedup stats: kept {num_documents_left_after_removal} out of {total_num_documents}")
 
     def run_transform(self):
         # read the document ids to remove from doc2remove folder
