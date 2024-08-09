1d54198c8d6b0cb1ebca6139ccddcac975477e25
Mohammad Nassar <mohammad.nassar@ibm.com>
2024-08-08 19:23:45 -0500

    Add READme.md file.
    Signed-off-by: Mohammad Nassar <mohammad.nassar@ibm.com>

--- kfp/superworkflows/ray/kfp_v2/superpipeline_noop_docId_v2.py
+++ kfp/superworkflows/ray/kfp_v2/superpipeline_noop_docId_v2.py
@@ -12,10 +12,9 @@ doc_id_image = "quay.io/dataprep1/data-prep-kit/doc_id-ray:latest"
 
 
 def _remove_unused_params(d: dict[str, Any]) -> None:
-    d.pop("input_parent_path", None)
-    d.pop("output_parent_path", None)
-    d.pop("parent_path_suffix", None)
-    d.pop("inter_output", None)
+    d.pop("input_path", None)
+    d.pop("output_path", None)
+    d.pop("intermediate_path", None)
     d.pop("skip", None)
     d.pop("name", None)
     d.pop("overriding_params", None)
@@ -23,8 +22,8 @@ def _remove_unused_params(d: dict[str, Any]) -> None:
 
 
 @dsl.component
-def prepare_params(input_prefix: str, inter_output_path: str) -> str:
-    data_s3_config = "{'input_folder': '" + input_prefix + "', 'output_folder': '" + inter_output_path + "'}"
+def prepare_params(input_path: str, output_path: str) -> str:
+    data_s3_config = "{'input_folder': '" + input_path + "', 'output_folder': '" + output_path + "'}"
     return data_s3_config
 
 
@@ -33,10 +32,9 @@ def super_pipeline(
     # the super pipeline parameters
     p1_pipeline_runtime_pipeline_id: str = "pipeline_id",
     p1_pipeline_server_url: str = "http://kuberay-apiserver-service.kuberay.svc.cluster.local:8888",
-    p1_pipeline_input_parent_path: str = "test/doc_id/input/",
-    p1_pipeline_output_parent_path: str = "test/super/output/",
-    # p1_pipeline_parent_path_suffix: str = "",
-    p1_pipeline_inter_output: str = "test/super/output/tmp",
+    p1_pipeline_input_path: str = "test/doc_id/input/",
+    p1_pipeline_output_path: str = "test/super/output/",
+    p1_pipeline_intermediate_path: str = "test/super/output/tmp",
     p1_pipeline_additional_params: str = '{"wait_interval": 2, "wait_cluster_ready_tmout": 400, "wait_cluster_up_tmout": 300, "wait_job_ready_tmout": 400, "wait_print_tmout": 30, "http_retries": 5}',
     p1_pipeline_data_s3_access_secret: str = "s3-secret",
     p1_pipeline_runtime_code_location: dict = {"github": "github", "commit_hash": "12345", "path": "path"},
@@ -52,8 +50,6 @@ def super_pipeline(
     p2_ray_name: str = "noop-kfp-ray",
     p2_ray_head_options: dict = {"cpu": 1, "memory": 4, "image_pull_secret": "", "image": noop_image},
     p2_ray_worker_options: dict = {"replicas": 2, "max_replicas": 2, "min_replicas": 2, "cpu": 2, "memory": 4, "image_pull_secret": "", "image": noop_image},
-    # overriding parameters
-    p2_overriding_params: dict = {"ray_worker_options": {"image": noop_image}, "ray_head_options": {"image": noop_image}},
     # Document ID step parameters
     p3_name: str = "doc_id",
     p3_ray_name: str = "docid-kfp-ray",
@@ -69,34 +65,36 @@ def super_pipeline(
     p3_doc_id_int_column: str = "int_id_column",
 ):
     args = locals()
-    common_params = {key: value for key, value in args.items() if key.startswith("p1_")}
-    task1_params = {key: value for key, value in args.items() if key.startswith("p2_")}
-    task2_params = {key: value for key, value in args.items() if key.startswith("p3_")}
     common_params_prefix = "p1_pipeline_"
-    common_prms_to_pass = {key[len(common_params_prefix) :]: value for key, value in common_params.items()}
+    transform1_prefix = "p2_"
+    transform2_prefix = "p3_"
+    # split the input parameters according to thier prefixes.
+    common_params = {key[len(common_params_prefix) :]: value for key, value in args.items() if key.startswith(common_params_prefix)}
+    task1_params = {key[len(transform1_prefix) :]: value for key, value in args.items() if key.startswith(transform1_prefix)}
+    task2_params = {key[len(transform2_prefix) :]: value for key, value in args.items() if key.startswith(transform2_prefix)}
 
-    #########################################
-    prefix1 = "p2_"
-    prefix2 = "p3_"
-    pipeline_prms_to_pass = common_prms_to_pass | {key[len(prefix1) :]: value for key, value in task1_params.items()}
+    # get the input path, output path of the whole pipeline, and the intermediate path for storing the files between the transforms
+    input_path = common_params.get("input_path", "")
+    output_path = common_params.get("output_path", "")
+    inter_path = common_params.get("intermediate_path", "")
 
+    # execute the first transform
+    pipeline_prms_to_pass = common_params | task1_params
     _remove_unused_params(pipeline_prms_to_pass)
-
-    input_prefix = common_params.get("p1_pipeline_input_parent_path", "")
-    parent_path_suffix = common_params.get("p1_pipeline_parent_path_suffix", "")
-    output_parent_path = common_params.get("p1_pipeline_output_parent_path", "")
-    inter_output_path = common_params.get("p1_pipeline_inter_output", "")
-
-    data_config = prepare_params(input_prefix=input_prefix, inter_output_path=inter_output_path)
+    # get the data config
+    data_config = prepare_params(input_path=input_path, output_path=inter_path)
     pipeline_prms_to_pass["data_s3_config"] = data_config.output
+    # call the noop pipeline from noop_wf.py file with the expected parameters
     noop_task = noop(**pipeline_prms_to_pass)
-    #########################################
-    
-    pipeline_prms_to_pass2 = common_prms_to_pass | {key[len(prefix2) :]: value for key, value in task2_params.items()}
-    _remove_unused_params(pipeline_prms_to_pass2)
-    data_config = prepare_params(input_prefix=inter_output_path, inter_output_path=output_parent_path)
-    pipeline_prms_to_pass2["data_s3_config"] = data_config.output
-    doc_id_task = doc_id(**pipeline_prms_to_pass2)
+
+    # execute the second transform
+    pipeline_prms_to_pass = common_params | task2_params
+    _remove_unused_params(pipeline_prms_to_pass)
+    # get the data config
+    data_config = prepare_params(input_path=inter_path, output_path=output_path)
+    pipeline_prms_to_pass["data_s3_config"] = data_config.output
+    # call the doc_id pipeline from doc_id_wf.py file with the expected parameters
+    doc_id_task = doc_id(**pipeline_prms_to_pass)
     doc_id_task.after(noop_task)
     
 
