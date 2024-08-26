Fixed bug in cluster calculation
Signed-off-by: nelson <kibnelson@gmail.com>

transforms/universal/fdedup/spark/src/fd_clusters_calculator.py
@@ -3,7 +3,7 @@ import logging
 import os
 
 from pyspark.sql import functions as F
-from pyspark.sql.functions import col, collect_set, desc, size
+from pyspark.sql.functions import col, collect_list, collect_set, desc, size
 from pyspark.sql.types import ArrayType, IntegerType, LongType, StructField, StructType
 from scipy.integrate import quad as integrate
 from spark_transformer_runtime import SparkFileBatcher, SparkTransformerRuntime
@@ -68,14 +68,15 @@ class FDClustersCalculator(SparkTransformerRuntime):
                 StructField("band_hash", LongType(), True),
                 StructField(
                     "document_data",
-                    ArrayType(StructType(
-                        [
-                            StructField(self.document_id_column, LongType(), True),
-                            StructField("minhashes", ArrayType(IntegerType()), True),
-                            StructField("document_length", IntegerType(), True),
-
-                        ]
-                    )),
+                    ArrayType(
+                        StructType(
+                            [
+                                StructField(self.document_id_column, LongType(), True),
+                                StructField("minhashes", ArrayType(IntegerType()), True),
+                                StructField("document_length", IntegerType(), True),
+                            ]
+                        )
+                    ),
                     True,
                 ),
                 StructField("cluster_size", IntegerType(), True),
@@ -102,6 +103,22 @@ class FDClustersCalculator(SparkTransformerRuntime):
                 num_bands_index = 0
         else:
             self.in_out_metadata[self.execution_name][self.execution_name + "_status"] = "progress"
+        sements_schema = StructType(
+            [
+                StructField("band_hash", LongType(), True),
+                StructField(
+                    "document_data",
+                    StructType(
+                        [
+                            StructField(self.document_id_column, LongType(), True),
+                            StructField("minhashes", ArrayType(IntegerType()), True),
+                            StructField("document_length", IntegerType(), True),
+                        ]
+                    ),
+                    True,
+                ),
+            ]
+        )
 
         for band_index in range(num_bands_index, num_bands):
             # track band index
@@ -112,6 +129,7 @@ class FDClustersCalculator(SparkTransformerRuntime):
 
                 # Create an initial empty DataFrame
                 band_df = self.spark.createDataFrame([], schema)
+                segments_df = self.spark.createDataFrame([], sements_schema)
                 bands_path = os.path.join(self.band_output_path, f"band={band_index}/segment={band_segment_index}")
                 self.input_files, self.file_stats = self.list_files(bands_path, self.file_ext)
                 file_batcher = SparkFileBatcher(
@@ -149,44 +167,44 @@ class FDClustersCalculator(SparkTransformerRuntime):
                     )
 
                     spark_df = self.read_data(input_batch, self.file_ext)
+
+                    segments_df = segments_df.union(spark_df)
                     logging.info(f"  Read {spark_df.count()} documents")
                     self.total_number_of_documents += spark_df.count()
-                    group_df = spark_df.groupBy("band_hash").agg(collect_set("document_data").alias("doc_ids"))
-                    # group_df = spark_df.groupBy("band_hash").agg(collect_set("int_id_column").alias("doc_ids"))
-                    group_df = group_df.withColumn("cluster_size", size(col("doc_ids")))
-                    group_df = group_df.orderBy(desc("cluster_size"))
-                    cluster_df = band_df.filter(col("cluster_size") > 1)
-                    logging.info(f"  Before filtering, {band_df.count()} clusters")
-                    band_df = band_df.union(group_df)
-                cluster_df = band_df.filter(col("cluster_size") > 1)
+                group_df = segments_df.groupBy("band_hash").agg(collect_set("document_data").alias("doc_ids"))
+                group_df = group_df.withColumn("cluster_size", size(col("doc_ids")))
+                group_df = group_df.orderBy(desc("cluster_size"))
+                cluster_df = group_df.filter(col("cluster_size") > 1)
                 logging.info(f"  Before filtering, {band_df.count()} clusters")
                 logging.info(f"  After filtering, {cluster_df.count()} clusters")
                 self.write_data(
                     cluster_df,
-                    os.path.join(self.clusters_doc_ids_output_path, f"cluster_bands={band_index}/segment={band_segment_index}"),
+                    os.path.join(
+                        self.clusters_doc_ids_output_path, f"cluster_bands={band_index}/segment={band_segment_index}"
+                    ),
                     self.file_ext,
                 )
 
     def execute(self):
-        # try:
-        # load metadata
-        self.in_out_metadata = self._load_metadata(self.output_path)
-        prefix, step_number, prev_step_name = self.extract_step_info(self.execution_name)
-        if (
-            self.execution_name in self.in_out_metadata
-            and self.execution_name + "_status" in self.in_out_metadata[self.execution_name]
-            and self.in_out_metadata[self.execution_name][self.execution_name + "_status"] == "complete"
-        ):
-            logging.info(f"Skipping {self.step_name} because its complete")
-        elif self.in_out_metadata[prev_step_name][prev_step_name + "_status"] == "complete":
-            self.run_transform()
-            self._save_metadata(self.execution_name, "complete", self.in_out_metadata, self.output_path)
-            logging.info("Finished grouping by band hash and band number")
-        else:
-            logging.info(f"Skipping {self.step_name} because the previous step failed")
-    # except Exception as ex:
-    #     self._save_metadata(self.execution_name, "error", self.in_out_metadata, self.output_path)
-    #     logging.error(f"Failed to group by band hash and band number: {ex}")
-    # finally:
-        self.stop()
-            # logging.info("Stopped the spark session for generating doc signatures")
+        try:
+            # load metadata
+            self.in_out_metadata = self._load_metadata(self.output_path)
+            prefix, step_number, prev_step_name = self.extract_step_info(self.execution_name)
+            if (
+                self.execution_name in self.in_out_metadata
+                and self.execution_name + "_status" in self.in_out_metadata[self.execution_name]
+                and self.in_out_metadata[self.execution_name][self.execution_name + "_status"] == "complete"
+            ):
+                logging.info(f"Skipping {self.step_name} because its complete")
+            elif self.in_out_metadata[prev_step_name][prev_step_name + "_status"] == "complete":
+                self.run_transform()
+                self._save_metadata(self.execution_name, "complete", self.in_out_metadata, self.output_path)
+                logging.info("Finished grouping by band hash and band number")
+            else:
+                logging.info(f"Skipping {self.step_name} because the previous step failed")
+        except Exception as ex:
+            self._save_metadata(self.execution_name, "error", self.in_out_metadata, self.output_path)
+            logging.error(f"Failed to group by band hash and band number: {ex}")
+        finally:
+            self.stop()
+            logging.info("Stopped the spark session for generating doc signatures")