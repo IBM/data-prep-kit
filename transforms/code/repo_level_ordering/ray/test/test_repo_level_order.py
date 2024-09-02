56e0e8fabe767b9e013fba39e388dfc158d7f1d5
Shivdeep Singh <Shivdeep.Singh@ibm.com>
2024-09-02 10:54:55 +0530

    Update repo level ordering transform
        - change flags to cli args
        - add tests
        - add batching while storing groups
        - optimise reading files in GroupByRepo
    
    Signed-off-by: Shivdeep Singh <Shivdeep.Singh@ibm.com>

--- transforms/code/repo_level_ordering/ray/test/test_repo_level_order.py
+++ transforms/code/repo_level_ordering/ray/test/test_repo_level_order.py
@@ -16,8 +16,31 @@ from data_processing.test_support.launch.transform_test import (
     AbstractTransformLauncherTest,
 )
 from data_processing_ray.runtime.ray import RayTransformLauncher
-from repo_level_order_transform_ray import RepoLevelOrderRayTransformConfiguration
+from repo_level_order_transform import RepoLevelOrderRayTransformConfiguration
 
 
-def test_sample_1():
-    pass
+class TestRepoLevelOrderingTransform(AbstractTransformLauncherTest):
+    """
+    Extends the super-class to define the test data for the tests defined there.
+    The name of this class MUST begin with the word Test so that pytest recognizes it as a test class.
+    """
+
+    def get_test_transform_fixtures(self) -> list[tuple]:
+        basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../test-data"))
+        config = {
+            "run_locally": True,
+            "repo_lvl_sorting_enabled": True,
+            "repo_lvl_sorting_algo": "SORT_SEMANTIC_NORMALISED",
+            "repo_lvl_store_type": "local",
+            "repo_lvl_store_backend_dir": "/tmp/mystore",
+            "repo_lvl_language_column": "language",
+        }
+        fixtures = [
+            (
+                RayTransformLauncher(RepoLevelOrderRayTransformConfiguration()),
+                config,
+                basedir + "/input",
+                basedir + "/expected",
+            )
+        ]
+        return fixtures
