12e5ef00020045568d6ff1ef5d143d32872535a2
blublinsky <blublinsky@hotmail.com>
2024-09-23 12:49:47 +0100

    completed Ray tests

--- data-processing-lib/python/test/data_processing_tests/transform/test_noop.py
+++ data-processing-lib/python/test/data_processing_tests/transform/test_noop.py
@@ -9,28 +9,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
-
-import pyarrow as pa
-from data_processing.test_support.transform import NOOPTransform
-from data_processing.test_support.transform import (
-    AbstractTableTransformTest,
+import os
+from data_processing.runtime.pure_python import PythonTransformLauncher
+from data_processing.test_support.launch.transform_test import (
+    AbstractTransformLauncherTest,
 )
+from data_processing.test_support.transform.noop_transform import NOOPPythonTransformConfiguration, sleep_cli_param
 
-
-table = pa.Table.from_pydict({"name": pa.array(["Tom", "Dick", "Harry"]), "age": pa.array([0, 1, 2])})
-expected_table = table  # We're a noop after all.
-expected_metadata_list = [{"nfiles": 1, "nrows": 3}, {}]  # transform() result  # flush() result
-
-
-class TestNOOPTransform(AbstractTableTransformTest):
+class TestPythonNOOPTransform(AbstractTransformLauncherTest):
     """
     Extends the super-class to define the test data for the tests defined there.
     The name of this class MUST begin with the word Test so that pytest recognizes it as a test class.
     """
 
     def get_test_transform_fixtures(self) -> list[tuple]:
-        fixtures = [
-            (NOOPTransform({"sleep": 0}), [table], [expected_table], expected_metadata_list),
-            (NOOPTransform({"sleep": 0}), [table], [expected_table], expected_metadata_list),
-        ]
+        src_file_dir = os.path.abspath(os.path.dirname(__file__))
+        fixtures = []
+        basedir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                               "../../../../../transforms/universal/noop/python/test-data"))
+        launcher = PythonTransformLauncher(NOOPPythonTransformConfiguration())
+        transform_config = {sleep_cli_param: 0}
+        fixtures.append(
+            (
+                launcher,
+                transform_config,
+                basedir + "/input",
+                basedir + "/expected",
+                [],  # optional list of column names to ignore in comparing test-generated with expected.
+            )
+        )
+
         return fixtures
