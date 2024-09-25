20ed36528fa6e2e3ae5bb659cd569f2425f80c82
aishwariyachakraborty <aishwariya.chakraborty@gmail.com>
2024-09-25 14:03:07 +0530

    HOSP init

---
+++ transforms/code/higher_order_syntactic_profiler/README.md
@@ -0,0 +1,11 @@
+# NOOP Transform 
+The NOOP transforms serves as a simple exemplar to demonstrate the development
+of a simple 1:1 transform.  Per the set of 
+[transform project conventions](../../README.md#transform-project-conventions)
+the following runtimes are available:
+
+* [python](python/README.md) - provides the base python-based transformation 
+implementation.
+* [ray](ray/README.md) - enables the running of the base python transformation
+in a Ray runtime
+
---
+++ transforms/code/higher_order_syntactic_profiler/python/src/hosp_transform_python.py
@@ -0,0 +1,45 @@
+# (C) Copyright IBM Corp. 2024.
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+import time
+
+from data_processing.runtime.pure_python import PythonTransformLauncher
+from data_processing.runtime.pure_python.runtime_configuration import (
+    PythonTransformRuntimeConfiguration,
+)
+from data_processing.utils import get_logger
+from transforms.code.higher_order_syntactic_profiler.python.src.hosp_transform import SemanticProfilerTransformConfiguration
+
+
+logger = get_logger(__name__)
+
+
+class SemanticProfilerPythonTransformConfiguration(PythonTransformRuntimeConfiguration):
+    """
+    Implements the PythonTransformConfiguration for SemanticProfiler as required by the PythonTransformLauncher.
+    SemanticProfiler does not use a RayRuntime class so the superclass only needs the base
+    python-only configuration.
+    """
+
+    def __init__(self):
+        """
+        Initialization
+        :param base_configuration - base configuration class
+        """
+        super().__init__(transform_config=SemanticProfilerTransformConfiguration())
+
+
+if __name__ == "__main__":
+    # launcher = SemanticProfilerRayLauncher()
+    launcher = PythonTransformLauncher(SemanticProfilerPythonTransformConfiguration())
+    logger.info("Launching sp transform")
+    launcher.launch()
