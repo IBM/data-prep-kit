768b785f36a32c9257cf8e22259c405958fd7499
Revital Sur <eres@il.ibm.com>
2024-10-02 12:05:38 -0500

    test Signed-off-by: Revital Sur <eres@il.ibm.com>

--- transforms/universal/ededup/kfp_ray/ededup_wf.py
+++ transforms/universal/ededup/kfp_ray/ededup_wf.py
@@ -93,7 +93,7 @@ def ededup(
     # data sampling
     ededup_n_samples: int = 10,
     # additional parameters
-    additional_params: str = '{"wait_interval": 2, "wait_cluster_ready_tmout": 400, "wait_cluster_up_tmout": 300, "wait_job_ready_tmout": 400, "wait_print_tmout": 30, "http_retries": 5, "delete_cluster_delay_minutes": 0}',
+    additional_params: str = '{"wait_interval": 2, "wait_cluster_ready_tmout": 400, "wait_cluster_up_tmout": 300, "wait_job_ready_tmout": 400, "wait_print_tmout": 30, "http_retries": 5, "delete_cluster_delay_minutes": 30}',
 ):
     """
     Pipeline to execute EDEDUP transform
