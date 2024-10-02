2bcc9b672ec44d62b1a3dc3f0e13e259a9d22aeb
Revital Sur <eres@il.ibm.com>
2024-10-02 03:16:37 -0500

    More change.
    Signed-off-by: Revital Sur <eres@il.ibm.com>
    Co-authored-by: Boris Lublinsky <blublinsky@ibm.com>

--- transforms/universal/ededup/kfp_ray/src/ededup_compute_execution_params.py
+++ transforms/universal/ededup/kfp_ray/src/ededup_compute_execution_params.py
@@ -97,7 +97,7 @@ def ededup_compute_execution_params(
     # Define number of workers
     n_workers = int((0.85 * (cluster_cpu - 1) - required_hash_cpu) / actor_cpu)
     print(f"Number of workers - {n_workers}")
-    if n_workers < 0:
+    if n_workers <= 0:
         print(f"Cluster is too small - estimated number of workers {n_workers}")
         sys.exit(1)
     # Limit amount of workers and processors to prevent S3 saturation
