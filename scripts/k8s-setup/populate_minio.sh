45bf59426b6ec4c02fb42eee2508beb761654963
Revital Sur <eres@il.ibm.com>
2024-10-09 23:22:07 +0300

    Minor fixes to repo_level_ordering kfp.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- scripts/k8s-setup/populate_minio.sh
+++ scripts/k8s-setup/populate_minio.sh
@@ -26,6 +26,7 @@ mc cp --recursive ${REPOROOT}/transforms/code/proglang_select/ray/test-data/inpu
 mc cp --recursive ${REPOROOT}/transforms/code/proglang_select/ray/test-data/languages/ kfp/test/proglang_select/languages
 mc cp --recursive ${REPOROOT}/transforms/code/malware/ray/test-data/input/ kfp/test/malware/input
 mc cp --recursive ${REPOROOT}/transforms/code/header_cleanser/ray/test-data/input/ kfp/test/header_cleanser/input
+mc cp --recursive ${REPOROOT}/transforms/code/repo_level_ordering/ray/test-data/input/ kfp/test/repo_level_ordering/input
 # language
 mc cp --recursive ${REPOROOT}/transforms/language/lang_id/ray/test-data/input/ kfp/test/lang_id/input
 mc cp --recursive ${REPOROOT}/transforms/language/doc_quality/ray/test-data/input/ kfp/test/doc_quality/input
