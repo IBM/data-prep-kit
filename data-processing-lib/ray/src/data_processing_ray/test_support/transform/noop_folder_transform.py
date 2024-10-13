a0a1400069fdf02f7fbec7b029a888fcb8ec4211
blublinsky <blublinsky@hotmail.com>
2024-10-13 08:53:49 +0100

    added noop Ray testing

--- data-processing-lib/ray/src/data_processing_ray/test_support/transform/noop_folder_transform.py
+++ data-processing-lib/ray/src/data_processing_ray/test_support/transform/noop_folder_transform.py
@@ -12,17 +12,30 @@
 
 
 from data_processing.test_support.transform import NOOPTransformConfiguration
+from data_processing.test_support.transform import NOOPFolderTransform
 from data_processing.utils import get_logger
 from data_processing_ray.runtime.ray import (
     RayTransformLauncher,
     RayTransformRuntimeConfiguration,
+    DefaultRayTransformRuntime
 )
+from data_processing.data_access import DataAccess
 
 
 logger = get_logger(__name__)
 
 
-class NOOPRayTransformConfiguration(RayTransformRuntimeConfiguration):
+class NOOPFolderPythonRuntime(DefaultRayTransformRuntime):
+    def get_folders(self, data_access: DataAccess) -> list[str]:
+        """
+        Get folders to process
+        :param data_access: data access
+        :return: list of folders to process
+        """
+        return [data_access.get_input_folder()]
+
+
+class NOOPFolderRayTransformConfiguration(RayTransformRuntimeConfiguration):
     """
     Implements the RayTransformConfiguration for NOOP as required by the RayTransformLauncher.
     NOOP does not use a RayRuntime class so the superclass only needs the base
@@ -33,11 +46,12 @@ class NOOPRayTransformConfiguration(RayTransformRuntimeConfiguration):
         """
         Initialization
         """
-        super().__init__(transform_config=NOOPTransformConfiguration())
+        super().__init__(transform_config=NOOPTransformConfiguration(clazz=NOOPFolderTransform),
+                         runtime_class=NOOPFolderPythonRuntime)
 
 
 if __name__ == "__main__":
     # launcher = NOOPRayLauncher()
-    launcher = RayTransformLauncher(NOOPRayTransformConfiguration())
+    launcher = RayTransformLauncher(NOOPFolderRayTransformConfiguration())
     logger.info("Launching noop transform")
     launcher.launch()
