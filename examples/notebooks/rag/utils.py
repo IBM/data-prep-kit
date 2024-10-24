c66c579be363c408a01c9d521d64cf2c2c2ee5d4
Qiragg <qiragg@gmail.com>
2024-10-24 02:03:27 -0700

    Add dpk connector
    Added notebook for DPK connector, requirements and utils

--- examples/notebooks/rag/utils.py
+++ examples/notebooks/rag/utils.py
@@ -2,7 +2,12 @@ import os
 import requests
 from humanfriendly import format_size
 import pandas as pd
+import magic
 import glob
+from dpk_connector.core.utils import (
+    urlparse_cached
+)
+from urllib.parse import unquote
 
 rootdir = os.path.abspath(os.path.join(__file__, "../../../../"))
 
@@ -70,3 +75,46 @@ def download_file(url, local_file, chunk_size=1024*1024):
         print(f"{local_file} ({file_size}) downloaded successfully.")
 ## --- end: download_file ------
 
+
+def get_mime_type(byte_data: bytes) -> str:
+    """
+    Obtain the MIME type for provided byte data using the magic library.
+
+    Args:
+        byte_data: bytes: Bytes data to identify mimetype for.
+        
+    Returns:
+        str: Mimetype for given bytes data.
+        
+    Example:
+        >>> byte_data = b'<!DOCTYPE html>\n...'
+        >>> get_mime_type(byte_data)
+        'text/html'
+    """
+    
+    # Validate input type
+    if not isinstance(byte_data, bytes):
+        raise TypeError("Input must be of type 'bytes'")
+
+    # Initialize a magic object for MIME type detection
+    mime = magic.Magic(mime=True)
+
+    # Return MIME type from the byte data
+    return mime.from_buffer(byte_data)
+
+
+def get_extension(url: str) -> str:
+    parsed = urlparse_cached(url)
+    path = parsed.path
+    filename = unquote(os.path.basename(path))
+    ext = os.path.splitext(filename)[1]
+    if 16 < len(ext):
+        ext = ext[:16]
+    return ext
+
+
+def get_filename_from_url(url: str) -> str:
+    parsed = urlparse_cached(url)
+    basename = unquote(os.path.splitext(os.path.basename(parsed.path))[0])
+    ext = get_extension(url)
+    return basename + ext
